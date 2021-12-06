d4592608922c93f4e0dc9fef3377f27157dd77cf
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-06 16:29:13 -0300

    Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Auth from "./Auth/Auth";
+import RegisterEdit from "./Register&Edit/Register&Edit";
 
 export default function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" pathname="/" element={<Auth />} />
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" pathname="/" element={<Auth/>}/>
+                <Route path="/edit" pathname="/edit" element={<RegisterEdit/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
--- src/Register&Edit/Register&Edit.js
+++ src/Register&Edit/Register&Edit.js
@@ -1,13 +1,7 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React from "react";
 
 
-function Auth() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-
-
+function RegisterEdit() {
     return (
         <div className="absolute w-1/2 h-1/2 transform translate-x-1/2 translate-y-1/2">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -17,7 +11,7 @@ function Auth() {
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text" placeholder="Usuário" value={username} onChange={(event) => setUsername(event.target.value)}/>
+                        type="text" placeholder="Usuário"/>
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -25,13 +19,13 @@ function Auth() {
                     </label>
                     <input
                         className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        type="password" placeholder="******************" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                        type="password" placeholder="******************"/>
                 </div>
                 <div className="flex items-center justify-between">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button" onClick={() => navigate('/dashboard')}>
-                        Entrar
+                        type="button">
+                        Sign In
                     </button>
                 </div>
             </form>
@@ -39,7 +33,7 @@ function Auth() {
                 &copy;2020 Example Corp. All rights reserved.
             </p>
         </div>
-);
+    );
 }
 
-export default Auth;
+export default RegisterEdit;

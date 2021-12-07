d9ab1ab1bbf0fba1d96411509a6e104ee0c7f19b
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-07 15:39:11 -0300

    Dashboard #1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Auth from "./Auth/Auth";
 import RegisterEdit from "./Register&Edit/Register&Edit";
+import Dashboard from "./Dashboard/Dashboard";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path="/" pathname="/" element={<Auth/>}/>
                 <Route path="/edit" pathname="/edit" element={<RegisterEdit/>}/>
+                <Route path="/dashboard" pathname="/dashboard" element={<Dashboard/>}/>
             </Routes>
         </BrowserRouter>
     );
